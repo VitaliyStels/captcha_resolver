18f198dbed88360fc2b809c7be05190f884c26d0
Vitalii Stels <stels12313@gmail.com>
2024-03-10 15:37:01 +0200

    Implemented Image to Text functionality

--- resolver.py
+++ resolver.py
@@ -1,10 +1,25 @@
 import os
-files = os.listdir('static/uploads')
+import pytesseract
+import cv2
 
-def check_image():
+dir = 'static/uploads'
+files = os.listdir(dir)
+
+async def check_image():
     if len(files) > 0:
         for i in files:
-            return f'{i} succeeded'
+            text = recognize_text(f'{dir}/{i}')
+            os.remove(f'{dir}/{i}')
+            print(f'{dir}/{i} was checked and removed')
+            return text
 
 
-    
+def recognize_text(image_path):
+
+  img = cv2.imread(image_path)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+  config = '--psm 6'  # Page segmentation mode (adjust based on image content)
+  text = pytesseract.image_to_string(thresh, config=config)
+
+  return text    
--- web.py
+++ web.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import render_template, flash, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 from dotenv import load_dotenv
 from resolver import check_image
+import asyncio
 load_dotenv()
 
 app = Flask(__name__)
@@ -21,25 +22,20 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
-    # Get the uploaded file
     image_file = request.files['image']
-    # Check if a file was uploaded
     if image_file and allowed_file(image_file.filename):
-        # Save the file
         filename = secure_filename(image_file.filename)
         image_file.save(os.path.join(UPLOAD_FOLDER, filename))
-
-        # You can further process the image here (e.g., resize, convert format)
-
-        # Optional: Flash a success message
-        flash('Image uploaded successfully!')
         return redirect('/process_image')
     else:
-        # Flash an error message
         return redirect(url_for('upload_image'))
 
+
 @app.route('/process_image', methods=['GET'])
-def return_prediction():
-    return render_template('index.html', text=check_image())
+async def return_prediction():
+    st = await check_image()
+    asyncio.sleep(1)
+    return render_template('succeed.html', text=st)
